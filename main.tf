6cbe7729839a4b2e65e56dc9ea98615fca3b0419
Krishna-Indium <103569977+Krishna-Indium@users.noreply.github.com>
2025-04-03 23:15:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
 # Create S3 Bucket Resource
+terraform {
+  backend "http" {
+    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/harnesspoc/workspaces/terraformkaja/terraform-backend?accountIdentifier=Zttn2VPHRki_xzEuBgsSbg"
+    username = "harness"
+    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/harnesspoc/workspaces/terraformkaja/terraform-backend/lock?accountIdentifier=Zttn2VPHRki_xzEuBgsSbg"
+    lock_method = "POST"
+    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/harnesspoc/workspaces/terraformkaja/terraform-backend/lock?accountIdentifier=Zttn2VPHRki_xzEuBgsSbg"
+    unlock_method = "DELETE"
+  }
+}
+
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   acl    = "public-read"
