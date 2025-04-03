dea234eff22bc57c98e54f184363b131bebbbb2d
Krishna-Indium <103569977+Krishna-Indium@users.noreply.github.com>
2025-04-03 23:28:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,9 +4,3 @@ variable "bucket_name" {
   description = "Name of the S3 bucket. Must be Unique across AWS"
   type        = string
 }
-
-variable "tags" {
-  description = "Tages to set on the bucket"
-  type        = map(string)
-  default     = {}
-}
